0b4313c80ea9742620b235b4f604dacba44610ce
hyunsoo <hyunsoo@hyunsooui-MacBookPro.local>
2026-07-12 17:55:07 +0900

    Update rag.py

--- vanilla_rag/rag.py
+++ vanilla_rag/rag.py
@@ -1,29 +1,24 @@
 """
 LangChain 라이브러리를 사용하지 않고 파이썬으로 구현한 RAG 파이프라인.
-
-
+보고서를 생성하는 LLM은 현재 'claude-haiku-4-5'이다.
 """
-
 
 from data.collect_data import research
 from data.dart_origin_document import get_disclosure_text
 from data.chunking import chunk_text
 from data.embedding import get_collection, store_chroma_db, check_disclosure_in_db, search
-
+from vanilla_rag.generate import build_context, generate_report
 
 if __name__ == "__main__":
     
     while True:
         corp_name = input("회사명: ")
         try:
-            information = research(corp_name)
+            question = input("질문: ")
+            information = research(corp_name=corp_name, query=question)
             break
         except ValueError as e:
             print(e)
-
-
-    question = input("질문: ")
-    
     
     corp_code = information.get("corp_code")
     collection = get_collection()
@@ -42,4 +37,12 @@ if __name__ == "__main__":
     # 검색
     results = search(collection=collection, query=question, corp_code=corp_code, n_results=3)
 
-    print("결과: \n", results)
+    # print("결과: \n", results)
+
+    # 보고서 생성
+    news = information.get("news", [[]])
+    context = build_context(results=results, news=news)
+    # print("context: \n", context)
+    report = generate_report(corp_name=corp_name, question=question, context=context)
+
+    print(report)
